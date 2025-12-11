18a8acabfa1f210520b54be8bea635b7dfe25264
Mario Tudón <90724520+MarioTudon@users.noreply.github.com>
2025-12-11 12:15:11 -0600

    Update goals.js

--- routes/goals.js
+++ routes/goals.js
@@ -6,11 +6,13 @@ export const createGoalsRouter = ({ goalsModel }) => {
   const goalsRouter = Router()
 
   const goalsController = new GoalsController({ goalsModel })
+  
+  goalsRouter.use(verifyAccessToken)
 
-  goalsRouter.get('/', verifyAccessToken, goalsController.getAll)
-  goalsRouter.post('/', verifyAccessToken, goalsController.create)
-  goalsRouter.patch('/:id', verifyAccessToken, goalsController.update)
-  goalsRouter.delete('/:id', verifyAccessToken, goalsController.delete)
+  goalsRouter.get('/', goalsController.getAll)
+  goalsRouter.post('/', goalsController.create)
+  goalsRouter.patch('/:id', goalsController.update)
+  goalsRouter.delete('/:id', goalsController.delete)
 
   return goalsRouter
 }
